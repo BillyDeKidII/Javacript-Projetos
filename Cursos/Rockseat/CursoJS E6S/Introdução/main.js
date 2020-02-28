cf113d49da50f1772707b16aa40c81b711a178ea
Thiago Romão <thiaroll@gmail.com>
2020-02-28 19:40:02 -0300

    Tudo upado

---
+++ Cursos/Rockseat/CursoJS E6S/Introdução/main.js
@@ -0,0 +1,7 @@
+//import soma, { sub } from './funcoes';
+import * as funcoes from './funcoes';
+
+console.log(funcoes.soma(1, 2));
+console.log(funcoes.sub(4, 2));
+console.log(funcoes.mult(4, 2));
+console.log(funcoes.centuryFromYear(1905));
