3bdfd8a66f3a919fa4f495ec6bd5e60eff5fb029
Thiago Romão <thiaroll@gmail.com>
2020-02-28 19:44:39 -0300

    Arrumando pra ter git commit

--- Cursos/Rockseat/CursoJS/JS Assíncrono/axios.js
+++
@@ -1,7 +0,0 @@
-axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    .then(function(response) {
-        console.log(response);
-    })
-    .catch(function(error) {
-        console.warn(error);
-    });
